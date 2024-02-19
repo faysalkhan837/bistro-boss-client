69d566ad7793d00d4c0de6c08a4eb0be5626508a
faysalkhan837 <faysal.khan837@gmail.com>
2024-02-20 03:31:14 +0600

    this is third updte

--- src/Components/Hooks/UseAxiousSecure.jsx
+++ src/Components/Hooks/UseAxiousSecure.jsx
@@ -1,11 +1,36 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import UseAuth from "./UseAuth";
 
 // eslint-disable-next-line react-refresh/only-export-components
- const axiousSecure = axios.create({
-    baseURL:'http://localhost:5000'
+const axiousSecure = axios.create({
+   baseURL: 'http://localhost:5000'
 })
 const UseAxiousSecure = () => {
-    
+   const navigate = useNavigate();
+   const { logOut } = UseAuth()
+   axiousSecure.interceptors.request.use(function (config) {
+      const token = localStorage.getItem('access-token')
+      console.log('request stoped by interseptor', token)
+      config.headers.authorization = `Bearer ${token}`
+      return config;
+   }, function (error) {
+      // do something with request error
+      return Promise.reject(error);
+   })
+   //  intercepts 401 and 403 status
+   axiousSecure.interceptors.response.use(function (response) {
+      return response;
+   }, async (error) => {
+      console.log(error)
+      const status = error.response.status;
+      // for 401 or 403 logOut the user and move to the login
+      if(status === 401 || status === 403){
+         await logOut();
+         navigate('/login');
+      }
+      return Promise.reject(error);
+   })
    return axiousSecure;
 };
 
